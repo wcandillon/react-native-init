c2aefc21ee9043e7f5e2301bf721e071ffb0a8da
William Candillon <wcandillon@gmail.com>
2019-06-08 22:51:17 +0200

    ⚙️ (#6)

--- bin/react-native-init.js
+++ bin/react-native-init.js
@@ -3,19 +3,32 @@ const sh = require('shelljs');
 const fs = require('fs');
 const path = require('path');
 
-const tsconfig = fs.readFileSync(path.resolve(__dirname, '../templates/tsconfig.json'), 'utf8');
 const eslintrc = fs.readFileSync(path.resolve(__dirname, '../templates/.eslintrc'), 'utf8');
-const gitignore = fs.readFileSync(path.resolve(__dirname, '../templates/.gitignore'), 'utf8');
-sh.exec('yarn add @types/react @types/react-native eslint eslint-config-react-native-wcandillon --dev');
-sh.mv('App.js', 'App.tsx');
-sh.config.silent = true;
-sh.echo(tsconfig).to('tsconfig.json');
 sh.echo(eslintrc).to('.eslintrc');
-sh.echo(gitignore).to('.gitignore');
+sh.exec('yarn add eslint eslint-config-react-native-wcandillon --dev');
 const package = JSON.parse(sh.cat('package.json'));
 package.scripts.lint = "eslint App.tsx components/*";
 package.scripts.tsc = "tsc";
 sh.echo(JSON.stringify(package, null, 2)).to('package.json');
-sh.config.silent = false;
+
+const tsConfig = JSON.parse(sh.cat('tsconfig.json'));
+tsConfig.compilerOptions = {
+  ...tsConfig.compilerOptions,
+  "strict": true,                           /* Enable all strict type-checking options. */
+  "noImplicitAny": true,                 /* Raise error on expressions and declarations with an implied 'any' type. */
+  "strictNullChecks": true,              /* Enable strict null checks. */
+  "strictFunctionTypes": true,           /* Enable strict checking of function types. */
+  "strictPropertyInitialization": true,  /* Enable strict checking of property initialization in classes. */
+  "noImplicitThis": true,                /* Raise error on 'this' expressions with an implied 'any' type. */
+  "alwaysStrict": true,                  /* Parse in strict mode and emit "use strict" for each source file. */
+
+  /* Additional Checks */
+  "noUnusedLocals": true,                /* Report errors on unused locals. */
+  "noUnusedParameters": true,            /* Report errors on unused parameters. */
+  "noImplicitReturns": true,             /* Report error when not all code paths in function return a value. */
+  "noFallthroughCasesInSwitch": true,    /* Report errors for fallthrough cases in switch statement. */
+  "forceConsistentCasingInFileNames": true,
+}
+sh.echo(JSON.stringify(tsConfig, null, 2)).to('tsconfig.json');
 
 console.log("done!");
